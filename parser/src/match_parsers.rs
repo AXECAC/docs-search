514e582b6132817b2239f574c7733ed8f471100e
aragami3070 <smirnov17612@gmail.com>
2026-02-17 13:07:07 +0400

    Feat(match_parsers): add read data from file

--- parser/src/match_parsers.rs
+++ parser/src/match_parsers.rs
@@ -3,8 +3,11 @@ use std::{str::from_utf8, sync::LazyLock};
 use infer::Infer;
 use mime::{Mime, TEXT_PLAIN};
 
-static INFER: LazyLock<Infer> = LazyLock::new(Infer::new);
+use crate::errors::ParserError;
 
+type Result<T> = std::result::Result<T, ParserError>;
+
+static INFER: LazyLock<Infer> = LazyLock::new(Infer::new);
 
 /// Определяет MIME файла по считанным данным
 pub fn define_mime_type(file_data: &[u8]) -> Option<Mime> {
@@ -19,4 +22,41 @@ pub fn define_mime_type(file_data: &[u8]) -> Option<Mime> {
     }
 
     None
+}
+
+fn read_data_from_file(file_name: &str) -> Result<Vec<u8>> {
+    Ok(std::fs::read(file_name)?)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn define_mime_docx_type() {
+        todo!()
+    }
+
+    #[test]
+    fn define_mime_docx_zip_type() {
+        todo!()
+    }
+
+    #[test]
+    fn define_mime_pdf_type() {
+        todo!()
+    }
+
+    #[test]
+    fn define_mime_pptx_type() {
+        todo!()
+    }
+
+    #[test]
+    fn define_mime_xlsx_type() {
+        todo!()
+    }
+
+    #[test]
+    fn define_mime_text_type() {
+        todo!()
+    }
 }
