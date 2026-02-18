4045d97c08473fc9cab1942f6164c99052189b76
aragami3070 <smirnov17612@gmail.com>
2026-02-18 10:43:01 +0400

    Feat(match_parsers): add tests for difne mime types

--- parser/src/match_parsers.rs
+++ parser/src/match_parsers.rs
@@ -24,39 +24,73 @@ pub fn define_mime_type(file_data: &[u8]) -> Option<Mime> {
     None
 }
 
+/// Считываем данные из файла ввиде byte vec
 fn read_data_from_file(file_name: &str) -> Result<Vec<u8>> {
     Ok(std::fs::read(file_name)?)
 }
 
 #[cfg(test)]
 mod tests {
+    use mime::TEXT_PLAIN;
+
+    use crate::{
+        constants::{APPLICATION_DOCX, APPLICATION_DOCX_ZIP, APPLICATION_PDF, APPLICATION_PPTX, APPLICATION_XLS, APPLICATION_XLSX},
+        match_parsers::{define_mime_type, read_data_from_file},
+    };
+
     #[test]
     fn define_mime_docx_type() {
-        todo!()
+        let data = read_data_from_file("assets/возможные вопросы теста.docx").unwrap();
+        let mime = define_mime_type(&data);
+        assert!(mime.is_some());
+        assert_eq!(mime.unwrap(), APPLICATION_DOCX);
     }
 
     #[test]
     fn define_mime_docx_zip_type() {
-        todo!()
+        let data = read_data_from_file("assets/6 сем англ.docx").unwrap();
+        let mime = define_mime_type(&data);
+        assert!(mime.is_some());
+        assert_eq!(mime.unwrap(), APPLICATION_DOCX_ZIP);
     }
 
     #[test]
     fn define_mime_pdf_type() {
-        todo!()
+        let data = read_data_from_file("assets/matematika._ch.2._tvimst.pdf").unwrap();
+        let mime = define_mime_type(&data);
+        assert!(mime.is_some());
+        assert_eq!(mime.unwrap(), APPLICATION_PDF);
     }
 
     #[test]
     fn define_mime_pptx_type() {
-        todo!()
+        let data = read_data_from_file("assets/Презентация_Часть1.pptx").unwrap();
+        let mime = define_mime_type(&data);
+        assert!(mime.is_some());
+        assert_eq!(mime.unwrap(), APPLICATION_PPTX);
+    }
+
+    #[test]
+    fn define_mime_xls_type() {
+        let data = read_data_from_file("assets/Сроки_зимней_сессии_и_пересдач_25_26.xls").unwrap();
+        let mime = define_mime_type(&data);
+        assert!(mime.is_some());
+        assert_eq!(mime.unwrap(), APPLICATION_XLS);
     }
 
     #[test]
     fn define_mime_xlsx_type() {
-        todo!()
+        let data = read_data_from_file("assets/Book.xlsx").unwrap();
+        let mime = define_mime_type(&data);
+        assert!(mime.is_some());
+        assert_eq!(mime.unwrap(), APPLICATION_XLSX);
     }
 
     #[test]
     fn define_mime_text_type() {
-        todo!()
+        let data = read_data_from_file("assets/main.typ").unwrap();
+        let mime = define_mime_type(&data);
+        assert!(mime.is_some());
+        assert_eq!(mime.unwrap(), TEXT_PLAIN);
     }
 }
