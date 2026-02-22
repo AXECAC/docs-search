d9a841f10f06dc5ca516653c5a34e9832d01b7c1
aragami3070 <smirnov17612@gmail.com>
2026-02-22 16:16:32 +0400

    Feat(errors): add tesseract errors with temp file in ParserError

--- parser/src/errors.rs
+++ parser/src/errors.rs
@@ -12,20 +12,35 @@ pub enum ParserError {
     #[error("IO error: {0}")]
     IoError(#[from] io::Error),
 
+    /// Ошибка создания временного файла
+    #[error("IO error: {0}")]
+    IoTempFileError(String),
+
     /// Ошибка чтения docx
     ///
     /// Ошибки библиотеки для работы с docx
     #[error("Docx error: {0}")]
     DocxError(#[from] docx_rs::ReaderError),
 
+    /// Ошибка tesseract::InitializeError
+    #[error("Tesseract init error: {0}")]
+    TesseractInitError(#[from] tesseract::InitializeError),
+
+    /// Ошибка tesseract::SetImageError
+    #[error("Tesseract set image error: {0}")]
+    TesseractSetImgError(#[from] tesseract::SetImageError),
+
+    /// Ошибка tesseract::plumbing::TessBaseApiGetUtf8TextError
+    #[error("Tesseract error: {0}")]
+    TessBaseApiGetUtf8TextError(#[from] tesseract::plumbing::TessBaseApiGetUtf8TextError),
+
     /// Файл с данным расширением не поддерживается
     #[error("Invalid format error: {0}")]
     InvalidFormat(String),
 }
 
-impl From<ParserError> for pyo3::PyErr  {
+impl From<ParserError> for pyo3::PyErr {
     fn from(value: ParserError) -> Self {
         PyValueError::new_err(value.to_string())
     }
-
 }
