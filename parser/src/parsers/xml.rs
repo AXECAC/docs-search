e726e1046d45943f059a2a8d0ed464a43fbbf049
aragami3070 <smirnov17612@gmail.com>
2026-02-24 01:17:00 +0400

    Feat(xml): add get info from docx xml rels fn
    Fn get map (target, id) of pictures from docx configuration file

---
+++ parser/src/parsers/xml.rs
@@ -0,0 +1,54 @@
+use crate::errors::ParserError;
+use quick_xml::Reader;
+use quick_xml::events::Event;
+use std::collections::HashMap;
+use std::io::{BufReader, Cursor};
+use zip::read::ZipFile;
+
+type Result<T> = std::result::Result<T, ParserError>;
+type Target = String;
+type Id = String;
+
+pub(crate) fn get_info_from_xml_rels(
+    mut reader: Reader<BufReader<ZipFile<'_, Cursor<&[u8]>>>>,
+) -> Result<HashMap<Target, Id>> {
+    let mut buf = Vec::new();
+    let mut images: HashMap<Target, Id> = HashMap::new();
+
+    loop {
+        match reader.read_event_into(&mut buf) {
+            Ok(Event::Empty(ref event)) | Ok(Event::Start(ref event)) => {
+                if event.name().as_ref() == b"Relationship" {
+                    let mut id = None;
+                    let mut target = None;
+                    let mut rel_type = None;
+
+                    for attr in event.attributes().with_checks(false).flatten() {
+                        match attr.key.as_ref() {
+                            b"Id" => id = Some(attr.unescape_value()?.to_string()),
+                            b"Target" => target = Some(attr.unescape_value()?.to_string()),
+                            b"Type" => rel_type = Some(attr.unescape_value()?.to_string()),
+                            _ => {}
+                        }
+                    }
+
+                    if let (Some(id), Some(mut target), Some(rel_type)) = (id, target, rel_type)
+                        && rel_type.ends_with("/image")
+                    {
+                        if target.starts_with('/') {
+                            target = target[1..].to_string()
+                        }
+                        images.insert(target, id);
+                    }
+                }
+            }
+            Ok(Event::Eof) => break,
+            Err(e) => return Err(ParserError::XmlError(e)),
+            _ => {}
+        }
+
+        buf.clear();
+    }
+
+    Ok(images)
+}
