80356607dd6d90002cd80cc173f0dfb2ddf97d46
aragami3070 <smirnov17612@gmail.com>
2026-02-25 10:55:58 +0400

    Feat(text): add extract from txt file test

--- parser/src/parsers/text.rs
+++ parser/src/parsers/text.rs
@@ -12,8 +12,32 @@ type Result<T> = std::result::Result<T, ParserError>;
 ///
 /// # Returns
 /// - Ok([`String`]) - возвращает текст
-/// - Err([`ParserError`]) - ошибка во время парсинга байтов текстового файла
+/// - Err([`ParserError::FromUTF8Error`]) - ошибка во время парсинга байтов текстового файла
 pub(crate) fn get_from_text(data: &[u8]) -> Result<String> {
     Ok(String::from_utf8(data.to_vec())?)
 }
 
+#[cfg(test)]
+mod tests {
+    use crate::{errors::ParserError, parsers::text::get_from_text};
+
+    type Result<T> = std::result::Result<T, ParserError>;
+
+    /// Считывает данные из файла ввиде byte vec
+    fn read_data_from_file(file_name: &str) -> Result<Vec<u8>> {
+        Ok(std::fs::read(file_name)?)
+    }
+
+    #[test]
+    fn extract_from_txt_file() -> Result<()> {
+        let data = read_data_from_file("assets/main.typ")?;
+        let res = get_from_text(&data)?;
+        assert_eq!(
+            res,
+            String::from_utf8(read_data_from_file(
+                "assets/tests_results/extract_from_txt_file.txt"
+            )?)?
+        );
+        Ok(())
+    }
+}
