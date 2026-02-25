fb4afe3226fd7e719968847691cceacf6bca25c2
aragami3070 <smirnov17612@gmail.com>
2026-02-25 09:40:55 +0400

    Chore(app): update main.py examples

--- app/main.py
+++ app/main.py
@@ -2,8 +2,7 @@ import docs_parser
 
 # NOTE: все эти точно работают и работают хорошо
 # print(docs_parser.get_text("parser/assets/text_and_tables.docx"))
+# print(docs_parser.get_text("parser/assets/some_text.docx"))
+# print(docs_parser.get_text("parser/assets/text_tables_png.docx"))
 # print(docs_parser.get_text("parser/assets/text_from_img.png"))
-# print(docs_parser.get_text("parser/assets/010300.docx"))
-# print(docs_parser.get_text("parser/assets/010300_new.docx"))
-print(docs_parser.get_text("parser/assets/text_tables_png.docx"))
-
+print(docs_parser.get_text("parser/assets/main.typ"))
