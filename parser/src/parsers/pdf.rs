5ebee1f89f03a1cc862323361e3e56a0a8176e8e
aragami3070 <smirnov17612@gmail.com>
2026-02-25 13:39:14 +0400

    Feat(pdf): add get from pdf fn

--- parser/src/parsers/pdf.rs
+++ parser/src/parsers/pdf.rs
@@ -2,3 +2,12 @@
 //!
 //! Для парсинга используется crate-ы pdf_extract
 
+use pdf_extract::extract_text_from_mem;
+
+use crate::errors::ParserError;
+
+type Result<T> = std::result::Result<T, ParserError>;
+
+pub(crate) fn get_from_pdf(data: &[u8]) -> Result<String> {
+    Ok(extract_text_from_mem(data)?)
+}
