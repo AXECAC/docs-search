5082c09b0810b3e7a9c729f36beca7a372431567
Anarchy73 <kanalcraba@gmail.com>
2026-02-27 09:19:21 +0400

    Add Fastapi example

--- app/main.py
+++ app/main.py
@@ -1,4 +1,17 @@
 import docs_parser
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
 
 # NOTE: все эти точно работают и работают хорошо
 # print(docs_parser.get_text("parser/assets/text_and_tables.docx"))
@@ -6,4 +19,4 @@ import docs_parser
 # print(docs_parser.get_text("parser/assets/text_tables_png.docx"))
 # print(docs_parser.get_text("parser/assets/text_from_img.png"))
 # print(docs_parser.get_text("parser/assets/main.typ"))
-print(docs_parser.get_text("parser/assets/main.pdf"))
+# print(docs_parser.get_text("parser/assets/main.pdf"))
