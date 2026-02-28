a89c99afa3cac91c1f3f0bd6a8aace6dc1e818fc
aragami3070 <smirnov17612@gmail.com>
2026-03-01 01:07:25 +0400

    Feat(image): remove useless using tempfile

--- parser/src/parsers/image.rs
+++ parser/src/parsers/image.rs
@@ -4,8 +4,7 @@
 
 use crate::{errors::ParserError, match_parsers};
 use mime::{IMAGE_BMP, IMAGE_JPEG, IMAGE_PNG, Mime};
-use std::io::{Cursor, Write};
-use tempfile::NamedTempFile;
+use std::io::Cursor;
 use tesseract::Tesseract;
 
 type Result<T> = std::result::Result<T, ParserError>;
@@ -16,39 +15,22 @@ type Result<T> = std::result::Result<T, ParserError>;
 /// извлекает любой текст с картинки
 ///
 /// # Arguments
-///
 /// - `data` - Слайс содержащий байты картинки (PNG, JPEG, etc.)
 ///
 /// # Returns
-///
 /// - Ok([`String`]) - ивзлеченный текст из картинки
 /// - Err([`ParserError`]) - ошибка во время парсинга или обработки картинки
 ///
 /// # Errors
-/// - [`ParserError::IoTempFileError`] - ошибка во время создания temp файла
 /// - [`ParserError::ImageError`] - ошибка во время обработки картинки
 /// - Остальные [`ParserError`] связанные с Tesseract ошибки во время парсинга картинки
-///
-/// # Implementation Notes
-///
-/// - Используется Tesseract OCR с поддержкой Английского и русского языка
-/// - Создается временный файл для передачи его в Tesseract
 pub(crate) fn get_from_image(data: &[u8]) -> Result<String> {
     let valid_data = match match_parsers::define_mime_type(data) {
         Some(mime) if is_correct_img_mime(&mime) => data,
         _ => &convert_to_png(data)?,
     };
 
-    // Создаем временный файл, для использования в OCR
-    let mut temp_file = NamedTempFile::new()?;
-    temp_file.write_all(valid_data)?;
-
-    let temp_file_path = temp_file
-        .path()
-        .to_str()
-        .ok_or_else(|| ParserError::IoTempFileError("".to_string()))?;
-
-    Ok(parse_with_tesseract(temp_file_path)?.trim_end().to_string())
+    Ok(parse_with_tesseract(valid_data)?.trim_end().to_string())
 }
 
 /// Проверка: является ли MIME тип поддерживаемым для парсинга
@@ -68,16 +50,14 @@ fn convert_to_png(data: &[u8]) -> Result<Vec<u8>> {
 /// Распознавание текста с помощью Tesseract OCR
 ///
 /// # Arguments
-///
 /// - `path` - путь до файла с картинкой
 ///
 /// # Returns
-///
 /// - Ok([`String`]) - извлеченный текст
 /// - Err([`ParserError`]) - если при работе с Tesseract возникает ошибка
-fn parse_with_tesseract(path: &str) -> Result<String> {
+fn parse_with_tesseract(data: &[u8]) -> Result<String> {
     // Инициализируем Tesseract с Английским и Русским языками
     let tes = Tesseract::new(None, Some("eng+rus"))?;
 
-    Ok(tes.set_image(path)?.get_text()?)
+    Ok(tes.set_image_from_mem(data)?.get_text()?)
 }
