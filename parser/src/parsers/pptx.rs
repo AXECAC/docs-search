23e01cc49cec666f7ff25ebb6bca1034d29309e1
aragami3070 <smirnov17612@gmail.com>
2026-03-07 10:09:55 +0400

    Feat(pptx): init PptxParser struct

--- parser/src/parsers/pptx.rs
+++ parser/src/parsers/pptx.rs
@@ -1,3 +1,20 @@
 //! Парсинг pptx файлов
 //!
-//! Для парсинга используется crate zip
+//! Для парсинга используется crate rustypptx
+
+use std::collections::HashMap;
+
+use crate::{errors::ParserError, parsers::image::get_from_image};
+
+type Result<T> = std::result::Result<T, ParserError>;
+type SlideIndex = u32;
+type ImgOnSlideNum = u32;
+type ImagesInfo = HashMap<(SlideIndex, ImgOnSlideNum), Vec<u8>>;
+
+pub(crate) struct PptxParser {
+    /// HashMap для сопоставления байтов картинки с её местом в тексте слайда
+    pub slides_img_info: ImagesInfo,
+    /// HashMap из индекса слайда и текста извлеченного из него
+    pub slides_text: HashMap<SlideIndex, String>,
+}
+
