36fcc02073e29990ad318b0624b83e2bc1421226
aragami3070 <smirnov17612@gmail.com>
2026-03-07 10:11:07 +0400

    Feat(pptx): add get from pptx method

--- parser/src/parsers/pptx.rs
+++ parser/src/parsers/pptx.rs
@@ -27,4 +27,72 @@ impl PptxParser {
         }
     }
 
+    /// Извлекает текстовые данные и текст из картинок
+    ///
+    /// # Arguments
+    /// - `data` - слайс байтов данных из файла
+    ///
+    /// # Returns
+    /// - Ok([`String`]) - возвращает текст
+    /// - Err([`ParserError`]) - ошибка во время парсинга pptx файла
+    ///
+    /// # Errors
+    /// - [`ParserError::PptxError`] - ошибка во время парсинга pptx
+    /// - [`ParserError::ImageError`] - ошибка во время парсинга картинки
+    /// - Остальные [`ParserError`] связанные с Tesseract ошибки во время парсинга картинки
+    pub(crate) fn get_from_pptx(mut self, data: &[u8]) -> Result<(String, ImagesInfo)> {
+        let pptx_doc = rustypptx::parse_pptx_bytes(data)?;
+
+        let mut result_text = String::new();
+        if let Some(title) = pptx_doc.metadata.title {
+            result_text.push_str(&format!("Название: {title}"));
+        }
+
+        for slide in pptx_doc.slides.iter() {
+            self.slides_img_info =
+                slide
+                    .images
+                    .iter()
+                    .enumerate()
+                    .fold(HashMap::new(), |mut info, (ind, img)| {
+                        info.insert((slide.index, ind as u32), img.data.clone());
+                        info
+                    });
+            self.slides_text =
+                slide
+                    .text_elements
+                    .iter()
+                    .fold(HashMap::new(), |mut sl_text, text_element| {
+                        sl_text.insert(slide.index, text_element.text.clone());
+                        sl_text
+                    })
+        }
+
+        result_text = self
+            .slides_text
+            .iter_mut()
+            .map(|(sl_ind, text)| {
+                text.push_str(
+                    &self
+                        .slides_img_info
+                        .iter()
+                        .filter(|((ind, _), _)| *ind == *sl_ind)
+                        .map(|((_, img_num), data)| {
+                            Ok(format!(
+                                "\n/********slide = {sl_ind}; img_num = {img_num}********/\n \
+                                {}\n \
+                                /****************************/",
+                                get_from_image(data)?
+                            ))
+                        })
+                        .collect::<Result<Vec<_>>>()?
+                        .join("\n"),
+                );
+                Ok(text.clone())
+            })
+            .collect::<Result<Vec<_>>>()?
+            .join("\n");
+
+        Ok((result_text, self.slides_img_info))
+    }
 }
