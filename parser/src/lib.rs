4e95518317730b5027f029160a0586d05d928772
aragami3070 <smirnov17612@gmail.com>
2026-03-08 15:48:33 +0400

    Feat(converter): init

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,4 +1,5 @@
 mod constants;
+mod converter;
 mod errors;
 mod match_parsers;
 mod parsers;
@@ -10,11 +11,17 @@ use pyo3::{PyResult, types::PyModule};
 mod parser {
     use pyo3::prelude::*;
 
-    /// Parsing text `from` file by `path`
     /// Парсинг текста `from` файла по `path`
     #[pyo3::pyfunction]
     pub fn get_text(from_path: &str) -> PyResult<String> {
         Ok(crate::match_parsers::get_text(from_path)?)
+    }
+
+    /// Парсинг текста `from` файла по `path`
+    #[pyo3::pyfunction]
+    pub fn convert_to_new_format(old_file_path: &str, new_path: &str) -> PyResult<String> {
+        // Ok(crate::match_parsers::get_text(from_path)?)
+        todo!()
     }
 }
 
@@ -22,5 +29,6 @@ mod parser {
 #[pymodule]
 fn docs_parser(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(parser::get_text, m)?)?;
+    m.add_function(wrap_pyfunction!(parser::convert_to_new_format, m)?)?;
     Ok(())
 }
