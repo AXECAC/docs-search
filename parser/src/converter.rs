c35b9695df8dee3eee557636944d3ebd9c1c3c4f
aragami3070 <smirnov17612@gmail.com>
2026-03-08 17:18:40 +0400

    Feat(converter): add calling soffice converter fn

--- parser/src/converter.rs
+++ parser/src/converter.rs
@@ -2,6 +2,8 @@
 //!
 //! Используется cli утилита поставляемая libreoffice - soffice
 
+use std::process::{Command, Stdio};
+
 use crate::{
     constants::{APPLICATION_DOC, APPLICATION_PPT, APPLICATION_RTF, APPLICATION_XLS},
     errors::ParserError,
@@ -10,7 +12,7 @@ use crate::{
 
 type Result<T> = std::result::Result<T, ParserError>;
 
-/// Перечисление старых форматов Microsoft office
+/// Поддерживаемые типы старых форматов Microsoft office
 enum MSOfficeFormat {
     /// doc like форматы
     Doc,
@@ -21,18 +23,27 @@ enum MSOfficeFormat {
 }
 
 /// Конвертер старых Microsoft office форматов в новые
+///
+/// Определяет формат и вызывает конвертацию файла
 /// # Arguments
 /// - `old_file_path` - путь по которому лежит файл старого формата
 /// - `new_path` - путь по которому должен появить файл нового формата
 ///
 /// # Errors
 /// - [`ParserError::InvalidFormat`] - тип файла не поддерживается/не определен
+/// - [`ParserError::IoError`] - проблемы с libreoffice
 pub(crate) fn convert_to_new_format(old_file_path: &str, new_path: &str) -> Result<()> {
     let file_data = read_data_from_file(old_file_path)?;
     match define_mime_type(&file_data) {
-        Some(mime) if mime == APPLICATION_RTF || mime == APPLICATION_DOC => todo!(),
-        Some(mime) if mime == APPLICATION_XLS => todo!(),
-        Some(mime) if mime == APPLICATION_PPT => todo!(),
+        Some(mime) if mime == APPLICATION_RTF || mime == APPLICATION_DOC => {
+            converter_files(MSOfficeFormat::Doc, old_file_path, new_path)
+        }
+        Some(mime) if mime == APPLICATION_XLS => {
+            converter_files(MSOfficeFormat::Xls, old_file_path, new_path)
+        }
+        Some(mime) if mime == APPLICATION_PPT => {
+            converter_files(MSOfficeFormat::Ppt, old_file_path, new_path)
+        }
         Some(mime) => Err(ParserError::InvalidFormat(format!(
             "Не поддерживается данный тип файла {mime}"
         ))),
@@ -40,4 +51,31 @@ pub(crate) fn convert_to_new_format(old_file_path: &str, new_path: &str) -> Resu
             "Не получается определить данный тип файла ".to_string(),
         )),
     }
+}
+
+/// Конвертирует файл в новый формат в зависимости от типа
+/// # Arguments
+/// - `old_file_path` - путь по которому лежит файл старого формата
+/// - `new_path` - путь по которому должен появить файл нового формата
+///
+/// # Errors
+/// - [`ParserError::IoError`] - проблемы с libreoffice
+fn converter_files(type_format: MSOfficeFormat, old_file_path: &str, new_path: &str) -> Result<()> {
+    let type_convert = match type_format {
+        MSOfficeFormat::Doc => "docx",
+        MSOfficeFormat::Xls => "xlsx",
+        MSOfficeFormat::Ppt => "pptx",
+    };
+
+    Command::new("soffice")
+        .arg("--headless")
+        .arg("--convert-to")
+        .arg(type_convert)
+        .arg(old_file_path)
+        .arg("--outdir")
+        .arg(new_path)
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .status()?;
+    Ok(())
 }
