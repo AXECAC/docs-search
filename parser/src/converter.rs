032e29aa04dc2f1bdb15518f6a9fff8e525cf63f
aragami3070 <smirnov17612@gmail.com>
2026-03-08 16:35:06 +0400

    Feat(converter): add doc comment

--- parser/src/converter.rs
+++ parser/src/converter.rs
@@ -10,6 +10,13 @@ use crate::{
 
 type Result<T> = std::result::Result<T, ParserError>;
 
+/// Конвертер старых Microsoft office форматов в новые
+/// # Arguments
+/// - `old_file_path` - путь по которому лежит файл старого формата
+/// - `new_path` - путь по которому должен появить файл нового формата
+///
+/// # Errors
+/// - [`ParserError::InvalidFormat`] - тип файла не поддерживается/не определен
 pub(crate) fn convert_to_new_format(old_file_path: &str, new_path: &str) -> Result<()> {
     let file_data = read_data_from_file(old_file_path)?;
     match define_mime_type(&file_data) {
