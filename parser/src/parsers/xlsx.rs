49c60bdb1688171227f2da249ef72602b4400a73
aragami3070 <smirnov17612@gmail.com>
2026-03-14 19:32:52 +0400

    Feat(xlsx): init XlsxParser struct

--- parser/src/parsers/xlsx.rs
+++ parser/src/parsers/xlsx.rs
@@ -1,3 +1,25 @@
 //! Модуль для парсинга xlsx файлов.
 //!
 //! Для парсинга используется crate-ы calamine и zip
+
+use std::{collections::HashMap, io::Cursor};
+
+use calamine::{Reader, Xlsx};
+use rayon::prelude::*;
+
+use crate::{
+    errors::ParserError,
+    parsers::{MSOfficParser, image::get_from_image},
+};
+
+type Result<T> = std::result::Result<T, ParserError>;
+type SheetIndex = u32;
+type ImgOnSheetNum = u32;
+type ImagesInfo = HashMap<(SheetIndex, ImgOnSheetNum), Vec<u8>>;
+
+pub(crate) struct XlsxParser {
+    /// HashMap для сопоставления байтов картинки с нужным sheet
+    pub sheet_img_info: ImagesInfo,
+    /// Текст sheet
+    pub sheet_text: Vec<String>,
+}
