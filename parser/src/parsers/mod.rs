ed349306c902cbb983bea6450f21c132e753de06
aragami3070 <smirnov17612@gmail.com>
2026-03-14 18:41:08 +0400

    Feat(parsers): add MSOfficeParser trait

--- parser/src/parsers/mod.rs
+++ parser/src/parsers/mod.rs
@@ -1,4 +1,8 @@
 //! Модуль для реализации парсеров
+
+use std::collections::HashMap;
+
+use crate::errors::ParserError;
 pub(crate) mod docx;
 pub(crate) mod image;
 pub(crate) mod pdf;
@@ -7,3 +11,13 @@ pub(crate) mod text;
 pub(crate) mod xlsx;
 
 mod xml;
+
+type Result<T> = std::result::Result<T, ParserError>;
+type ImgNum = u32;
+type Bytes = u8;
+type ImagesInfo = HashMap<(u32, ImgNum), Vec<Bytes>>;
+
+/// Trait для парсеров MS office с извлечением текста и извлечением текста с изображений
+pub(crate) trait MSOfficParser {
+    fn get_text(self, data: &[Bytes]) -> Result<(String, ImagesInfo)>;
+}
