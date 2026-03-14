3e75daa51ba765a8195c7922e25bebbc0209de84
aragami3070 <smirnov17612@gmail.com>
2026-03-14 19:36:08 +0400

    Feat(xlsx): impl MSOfficeParser with get_text method
    Only text extraction

--- parser/src/parsers/xlsx.rs
+++ parser/src/parsers/xlsx.rs
@@ -23,6 +23,55 @@ pub(crate) struct XlsxParser {
     /// Текст sheet
     pub sheet_text: Vec<String>,
 }
+
+impl MSOfficParser for XlsxParser {
+    /// Извлекает текстовые данные и текст из картинок
+    ///
+    /// # Arguments
+    /// - `mut `[`self`] - сам парсер (забирает владение над парсером)
+    /// - `data` - слайс байтов данных из файла
+    ///
+    /// # Returns
+    /// - Ok([`String`]) - возвращает текст
+    /// - Err([`ParserError`]) - ошибка во время парсинга xlsx файла
+    ///
+    /// # Errors
+    /// - [`ParserError::XlsxError`] - ошибка во время парсинга xlsx
+    /// - [`ParserError::ImageError`] - ошибка во время парсинга картинки
+    /// - Остальные [`ParserError`] связанные с Tesseract ошибки во время парсинга картинки
+    fn get_text(mut self, data: &[u8]) -> Result<(String, ImagesInfo)> {
+        let cursor = Cursor::new(data);
+
+        let mut excel = Xlsx::new(cursor)?;
+
+        let sheet_names = excel.sheet_names().clone();
+
+        for name in sheet_names {
+            if let Ok(range) = excel.worksheet_range(&name) {
+                let mut cur_sheet_text = String::new();
+                cur_sheet_text.push_str("\n*** Sheet: ");
+                cur_sheet_text.push_str(&name);
+                cur_sheet_text.push_str(" ***\n");
+                cur_sheet_text.push_str(
+                    &range
+                        .rows()
+                        .map(|row| {
+                            row.iter()
+                                .map(std::string::ToString::to_string)
+                                .collect::<Vec<String>>()
+                                .join(",")
+                        })
+                        .collect::<Vec<String>>()
+                        .join("\n"),
+                );
+                self.sheet_text.push(cur_sheet_text);
+            }
+        }
+
+        Ok((self.sheet_text.join("\n"), self.sheet_img_info))
+    }
+}
+
 impl XlsxParser {
     /// Создает новый [`XlsxParser`].
     pub(crate) fn new() -> Self {
