3192dbb873380156d1fd78608ec29fa358dae6b2
Anarchy73 <kanalcraba@gmail.com>
2026-03-22 18:59:19 +0400

    feat(app): add upload endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import docs_parser
-from fastapi import FastAPI
-
+from fastapi import FastAPI, File, UploadFile
+from schemas import ParseResponse
+import os
+import uuid
 app = FastAPI()
 
 
@@ -12,6 +14,26 @@ def read_root():
 @app.get("/items/")
 def read_item(source: str):
     return {source: docs_parser.get_text(source)}
+
+@app.post("/upload", response_model=ParseResponse)
+async def upload_file(file: UploadFile,
+                      parse_immediatly: bool = False):
+    file_id = uuid.uuid4()
+    file_path = f"uploads/raw/{file_id}_{file.filename}"
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+    content = file.read()
+    with open(file_path, "wb") as f:
+        f.write(content)
+
+    parsed_path = None
+    return ParseResponse(
+        file_id=file_id,
+        original_filename=file.filename,
+        parsed_file_path=parsed_path,
+        status = "parsed" if parse_immediatly else "uploaded"
+    )
+
 
 # NOTE: все эти точно работают и работают хорошо
 # print(docs_parser.get_text("parser/assets/text_and_tables.docx"))
