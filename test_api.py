762b5bffcd16a0146e0d37ab8496577c7fe172c2
Nomen Conservandum <hramov.sanya2018@yandex.ru>
2026-06-14 18:48:30 +0400

    Remove temporary patch and test scripts

--- test_api.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-r = requests.post("http://localhost:8000/auth/login", data={"username": "admin", "password": "password"})
-token = r.json().get("access_token")
-
-res = requests.get("http://localhost:8000/documents", headers={"Authorization": f"Bearer {token}"})
-print(json.dumps(res.json(), indent=2))
